c4e61add60b26c50473cc09d3092cef4a61530a1
ryansummerlin <ryansummerlin777@gmail.com>
2023-07-06 12:21:56 -0700

    finished the practice

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-
+import fruitReducer from './fruitReducer';
 // `combineReducers` combines all the reducer functions into one big reducer
 // function, which is typically called `rootReducer`. This is the most important
 // part of this file. You will add your reducers here to work with your various
 // components.
 
 // create rootReducer here:
-
+const rootReducer = combineReducers({
+  fruitState: fruitReducer
+})
 // `enhancer` allows you to alter the store and add functionality such as the
 // Redux DevTools and logger (similar to morgan) middleware
 let enhancer;
@@ -31,9 +33,9 @@ if (process.env.NODE_ENV !== 'production') {
   enhancer = composeEnhancers(applyMiddleware(logger));
 }
 
-// `createStore` creates a store object literal {} 
+// `createStore` creates a store object literal {}
 // `preloadedState`--not important for now--is mainly used for hydrating state
-// from the server. 
+// from the server.
 // For `enhancer`, see above.
 // `configureStore` is the variable you will use in your root index.js to give
 // the Redux store access to the full application.
